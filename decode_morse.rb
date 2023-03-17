0b0edf6c7489c22fdef0d3d4cd2b5d4462e0305e
SabiMantock <sabimantock@gmail.com>
2023-03-17 12:40:19 +0000

    fixed linter errors

--- decode_morse.rb
+++ decode_morse.rb
@@ -31,21 +31,20 @@ def decode_char(morse_code)
   MORSE_CODE[morse_code]
 end
 
-puts decode_char('--')
-
+def decode_word(morse_code_word)
+  morse_characters = morse_code_word.split
+  decoded_characters = morse_characters.map do |morse_character|
+    decode_char(morse_character)
+  end
+  decoded_characters.join
+end
 
 def decode_sentence(morse_code_sentence)
   morse_words = morse_code_sentence.split('   ')
   decoded_words = morse_words.map do |morse_word|
     decode_word(morse_word)
   end
-  decoded_words.join(' ')
+  decoded_words.join(' ')
 end
 
-def decode_word(morse_code_word)
-  morse_characters = morse_code_word.split
-  decoded_characters = morse_characters.map do |morse_character|
-    decode_word(morse_word)
-  end
-  decoded_characters.join
-end
+puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
